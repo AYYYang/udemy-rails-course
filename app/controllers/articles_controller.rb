f6ef391f2064ec0dab386b042ed9f011f8c56b2b
Amanda Yang <amanda.yang@clio.com>
2021-02-12 10:01:22 -0800

    added articles listener

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id]) # instance variable, now it's aval in show
     end
 
+    def index
+        @articles = Article.all
+
+    end
+
 end
