6421571f6310a980a1fcdba4e759732969cc6589
Amanda Yang <amanda.yang@clio.com>
2021-02-12 16:25:55 -0800

    refactored working ugly blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,55 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show
-        @article = Article.find(params[:id]) # instance variable, now it's aval in show
     end
 
     def index
         @articles = Article.all
-
     end
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "article was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "edits saved."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        if @article.destroy
+            flash[:notice] = "Article #{@article.title} deleted."
+        end
+        redirect_to articles_path
+    end
+
+    private 
+    
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+    
 
 end
