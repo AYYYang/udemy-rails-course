b47cebf9b4934b24ad39b4ed94ae9376f0c3805f
Amanda Yang <amanda.yang@clio.com>
2021-03-09 10:44:15 -0800

    migration of user and article and fixsed front end username display

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum:3, maximum:100}
-  validates :description, presence: true, length: {minimum:10, maximum:300}
+  belongs_to :user, foreign_key: "user_id"
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
                     length: { maximum: 105 },
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
+
+  has_many :articles
 end
