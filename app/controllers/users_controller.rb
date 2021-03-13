bd5d9af293a838dcd0e612dda48a37475acab455
Amanda Yang <amanda.yang@clio.com>
2021-03-12 16:58:34 -0800

    add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "user was created successfully."
-      # redirect_to @user
+      flash[:notice] = "Welcome to the blog #{@user.username}! You have successfully signed up."
+      redirect_to articles_path
     else
       render "new"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
